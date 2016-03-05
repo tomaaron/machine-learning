1ce1eb63ff1b7205e5b29850dbd63b777dc19782
Tom Aaron Koch <tom.a.koch@campus.tu-berlin.de>
2016-03-05 14:21:53 +0100

    kmeans finished

--- k_means.py
+++ k_means.py
@@ -8,8 +8,21 @@ def assign_to_means(d,means):
 	for i, m in enumerate(means):
 		dist[:,i] = map(lambda x: s_eucl_distance(x,m),d)
 	labels =np.argmin(dist,axis=1)
-	return dist,labels
+	return labels
 def calculate_new_means(labels,d,means):
+	new_means = np.empty_like(means)
 	for i, m in enumerate(means):
-		means[i] = np.mean(d[labels==i],axis=0)
-	return means
+		if(len(d[labels==i]) > 0):
+			new_means[i] = np.mean(d[labels==i],axis=0)
+	return new_means
+def kmeans(data,k):
+	means_old = random_means(data,k)
+	d = sys.maxint 
+	labels = assign_to_means(data,means_old)
+	epsilon = 1
+	while(d > epsilon):
+		means_new = calculate_new_means(labels,data,means_old)
+		labels = assign_to_means(data,means_new)
+		d = s_eucl_distance(means_old,means_new)
+		means_old = means_new
+	return means_old
